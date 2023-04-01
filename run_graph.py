0842d888aabfc49225d375d0f3065056a127722e
codeuntilcold <quocdungngole@gmail.com>
2023-04-01 20:08:08 +0700

    add a bucket to contain surronding frames and drip out the most common

--- run_graph.py
+++ run_graph.py
@@ -3,11 +3,14 @@ import networkx as nx
 import pandas as pd
 import socket
 import logging
+import statistics
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(message)s',
                     datefmt="%y-%m-%d %H:%M:%S")
 
+RED = '\033[0;31m'
+NC = '\033[0m'  # No Color
 ACTIONS = [
     "open phone box",
     "take out phone",
@@ -19,60 +22,15 @@ ACTIONS = [
     "put in instruction paper",
     "inspect phone",
     "put in phone",
-    "close phone box"
+    "close phone box",
+    "no action"
 ]
-
 EDGE_NOT_EXIST = 0.01
-MIN_TRANS_PROB = 0.05
-
-G = nx.from_pandas_edgelist(pd.read_csv('trans.txt', sep=' '), 
-                            source='from', target='to', edge_attr='prob', 
+MIN_TRANS_PROB = 0.01
+DEFAULT_CONF = 0.5
+G = nx.from_pandas_edgelist(pd.read_csv('trans.txt', sep=' '),
+                            source='from', target='to', edge_attr='prob',
                             create_using=nx.DiGraph())
-
-
-class TransitionGraph:
-    def __init__(self, stream):
-        self.action_stream = stream
-        self.current_state = None
-        self.previous_conf = 0.01
-
-    def update_state(self):
-        try:
-            new_state, confidence = next(self.action_stream)
-            new_state, confidence = int(new_state), float(confidence)
-        except ValueError:
-            logging.info("=== END OF ACTION ===")
-            self.current_state = None
-            return True
-        except Exception:
-            logging.error("Stop updating state...")
-            return False
-        
-        if new_state not in range(len(ACTIONS)):
-            logging.info(f"Ignore invalid state {new_state}")
-            return True
-
-        trans_prob = G.get_edge_data(self.current_state, new_state, 
-                                    { 'prob': EDGE_NOT_EXIST })['prob']
-        trans_prob *= self.previous_conf
-
-        if self.current_state != new_state:
-            if self.current_state is None:
-                logging.info("=== START NEW ACTION ===")
-                logging.info(f"\t\t[{new_state}] {ACTIONS[new_state]}")
-                self.current_state = new_state
-            elif trans_prob > MIN_TRANS_PROB:
-                logging.info(f"({trans_prob:3f})\t[{new_state}] {ACTIONS[new_state]}")
-                self.current_state = new_state
-            else:
-                RED='\033[0;31m'
-                NC='\033[0m' # No Color
-                logging.info(f"{RED}({trans_prob:3f})\t[{new_state}] {ACTIONS[new_state]}{NC}")
-                # self.current_state = new_state
-                # pass
-
-        self.previous_conf = confidence
-        return True
 
 
 class ModelConnector:
@@ -81,19 +39,15 @@ class ModelConnector:
         self.serversocket.bind((socket.gethostname(), graph_port))
         self.serversocket.listen(5)
         self.clientsocket = None
-    
+
     def get_stream_gen(self):
         while True:
             # accept new client
             if input("Accept new client? [y/n] ").lower() == 'n':
                 break
-            
-            logging.info("Waiting for new client")
-
             # blocking operation
             self.clientsocket, addr = self.serversocket.accept()
             logging.info(f"Connection from {addr}")
-
             while True:
                 data = self.clientsocket.recv(1024).decode()
                 if not data:
@@ -107,6 +61,71 @@ class ModelConnector:
         if self.clientsocket:
             self.clientsocket.close()
         self.serversocket.close()
+
+
+class Bucket:
+    def __init__(self, stream, radius=3):
+        # past         👇 current        future
+        # [0    1   2   3   4   5   6   7]
+        self.stream = stream
+        self.radius = radius
+        self.bucket = list([None] * (2 * radius + 1))
+        self.conf = list([DEFAULT_CONF] * (2 * radius + 1))
+
+    def fill(self):
+        try:
+            new_state, confidence = next(self.stream)
+            new_state, confidence = int(new_state), float(confidence)
+        except ValueError:
+            new_state, confidence = None, DEFAULT_CONF
+        except Exception as e:
+            raise e
+
+        if new_state not in range(len(ACTIONS)):
+            logging.info(f"Ignore invalid state {new_state}")
+        else:
+            self.bucket = self.bucket[1:] + [new_state]
+
+    def get_mode(self):
+        return statistics.multimode(self.bucket)[-1]
+
+    def get_prev_conf(self):
+        return self.conf[self.radius - 1]
+
+    def get_conf(self):
+        return self.conf[self.radius]
+
+    def drip(self):
+        self.fill()
+        return self.get_mode(), self.get_prev_conf(), self.get_conf()
+
+
+class TransitionGraph:
+    def __init__(self):
+        self.current_state = None
+
+    def update_state(self, new_state, confidence):
+        trans_prob = G.get_edge_data(self.current_state, new_state,
+                                     {'prob': EDGE_NOT_EXIST})['prob']
+        trans_prob *= confidence
+
+        if self.current_state != new_state:
+            if self.current_state is None:
+                logging.info("=== START NEW ACTION ===")
+                logging.info(f"\t\t[{new_state}] {ACTIONS[new_state]}")
+            elif new_state is None:
+                logging.info("===  END OF ACTION   ===")
+            elif trans_prob > MIN_TRANS_PROB:
+                logging.info(
+                    f"({trans_prob:3f})\t[{new_state}] {ACTIONS[new_state]}")
+            else:
+                # still take mistake actions into account
+                logging.info(
+                    f"{RED}({trans_prob:3f})\t[{new_state}] {ACTIONS[new_state]}{NC}")
+
+            self.current_state = new_state
+
+        return True
 
 
 def notify():
@@ -123,25 +142,32 @@ def notify():
 
 def main():
     connector = ModelConnector()
-    bone = TransitionGraph(connector.get_stream_gen())
+    bucket = Bucket(stream=connector.get_stream_gen())
+    bone = TransitionGraph()
 
-    while bone.update_state():
+    while True:
+        new_state, prev_conf, conf = bucket.drip()
+        continue_process = bone.update_state(new_state, prev_conf)
+        if not continue_process:
+            break
+
         # visualize transition graph, very slow
         if False:
-            edges,weights = zip(*nx.get_edge_attributes(G,'prob').items())
+            edges, weights = zip(*nx.get_edge_attributes(G, 'prob').items())
             plt.clf()
             pos = nx.spring_layout(G, seed=42)
-            nx.draw(G, pos, 
+            nx.draw(G, pos,
                     node_color=[
-                        "r" if n == bone.current_state else "w" for n in G.nodes()], 
-                    width=2, 
+                        "r" if n == bone.current_state else "w" for n in G.nodes()],
+                    width=2,
                     with_labels=True,
                     edgelist=edges,
-                    edge_color=weights, 
+                    edge_color=weights,
                     edge_cmap=plt.cm.Blues)
             plt.pause(1 / 64)
 
     connector.close()
+
 
 if __name__ == "__main__":
     main()
