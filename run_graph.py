8ce5d1f33f93a65053c36517f4360fe0ff446523
codeuntilcold <quocdungngole@gmail.com>
2023-04-04 16:31:26 +0700

    add report and saving to text files

--- run_graph.py
+++ run_graph.py
@@ -1,3 +1,6 @@
+import os
+from time import time
+import json
 import matplotlib.pyplot as plt
 import networkx as nx
 import pandas as pd
@@ -10,7 +13,7 @@ logging.basicConfig(level=logging.INFO,
                     datefmt="%y-%m-%d %H:%M:%S")
 
 RED = '\033[0;31m'
-NC = '\033[0m'  # No Color
+NC = '\033[0m'
 ACTIONS = [
     "open phone box",
     "take out phone",
@@ -26,6 +29,7 @@ ACTIONS = [
     "no action"
 ]
 NO_ACTION_LABEL = len(ACTIONS) - 1
+END_ACTION_LABEL = 10
 EDGE_NOT_EXIST = 0.01
 MIN_TRANS_PROB = 0.01
 DEFAULT_CONF = 0.5
@@ -36,7 +40,7 @@ G = nx.from_pandas_edgelist(pd.read_csv('trans.txt', sep=' '),
 
 class ClientDisconnected(Exception):
     pass
-    
+
 
 class ModelConnector:
     def __init__(self, graph_port=24000):
@@ -96,6 +100,7 @@ class Bucket:
             logging.warn(
                 f"Invalid state {new_state}, default to [{NO_ACTION_LABEL}] {ACTIONS[NO_ACTION_LABEL]}")
             new_state = NO_ACTION_LABEL
+
         self.bucket = self.bucket[1:] + [new_state]
         self.conf = self.conf[1:] + [confidence]
 
@@ -120,10 +125,63 @@ class Bucket:
         self.conf = [DEFAULT_CONF for _ in self.conf]
 
 
+class ActionReport:
+    def __init__(self, action_id, starttime, endtime=-1, is_mistake=False):
+        self.worker_id = 0
+        self.action_id = action_id
+        self.starttime = starttime
+        self.duration = endtime - starttime
+        self.is_mistake = is_mistake
+
+    def set_endtime(self, endtime):
+        self.duration = endtime - self.starttime
+        return self
+    
+    def toggle_mistake(self):
+        self.is_mistake = not self.is_mistake
+        return self
+
+    def to_dict(self):
+        return {
+            "worker_id": self.worker_id,
+            "action_id": self.action_id,
+            "starttime": self.starttime,
+            "duration": self.duration,
+            "is_mistake": self.is_mistake
+        }
+
+
+class AssemblyReport:
+    def __init__(self):
+        self.id = 0
+        self.report: list[ActionReport] = list()
+
+    def add(self, action):
+        self.report.append(action)
+
+    def last(self) -> ActionReport:
+        # TODO: Will panic if list has length 0
+        return self.report[-1]
+
+    def save(self):
+        os.mkdir("report")
+        with open(f"report/{int(self.report[0].starttime)}.txt", 'w') as f:
+            f.write(json.dumps(self.to_dict(), indent=2))
+
+    def clear(self):
+        self.report = list()
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "report": [act.to_dict() for act in self.report]
+        }
+
 
 class TransitionGraph:
     def __init__(self):
         self.current_state = NO_ACTION_LABEL
+        self.report = AssemblyReport()
 
     def update_state(self, new_state, confidence):
         if self.current_state == new_state:
@@ -132,22 +190,40 @@ class TransitionGraph:
         trans_prob = G.get_edge_data(self.current_state, new_state,
                                      {'prob': EDGE_NOT_EXIST})['prob']
         trans_prob *= confidence
-
+        current_time = time()
         state_and_label = f"[{new_state}] {ACTIONS[new_state]}"
+
         if self.current_state == NO_ACTION_LABEL:
-            logging.info("=== START NEW ACTION ===")
-            logging.info(f"\t\t{state_and_label}")
+            logging.info(f"Start action: {state_and_label}")
+            # TODO: How to determine whether this is a mistake
+            self.report.add(ActionReport(new_state, current_time))
+
         elif new_state == NO_ACTION_LABEL:
-            logging.info("===  END OF ACTION   ===")
+            logging.info("Stop action")
+            self.report.last().set_endtime(current_time)
+            if self.current_state == END_ACTION_LABEL:
+                self.report.save()
+                self.report.clear()
+
         elif trans_prob > MIN_TRANS_PROB:
             logging.info(f"({trans_prob:3f})\t{state_and_label}")
+            self.report.last().set_endtime(current_time)
+            # TODO: End time and start time collide
+            self.report.add(ActionReport(new_state, current_time))
+
         else:
             logging.info(f"{RED}({trans_prob:3f})\t{state_and_label}{NC}")
+            self.report.last().set_endtime(current_time)
+            # TODO: End time and start time collide
+            self.report.add(ActionReport(new_state, current_time))
+            self.report.last().toggle_mistake()
 
         self.current_state = new_state
-    
+
     def reset_state(self):
         self.current_state = NO_ACTION_LABEL
+        self.report.save()
+        self.report.clear()
 
 
 def notify():
